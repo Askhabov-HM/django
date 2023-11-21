6c538a0e003353b6a506675f078603541ecb9840
Askhabov-HM <askhamzat@mail.ru>
2023-11-21 23:07:33 +0300

    #T-4 backend(Feature): create todo app models

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+
+    def __str__(self):
+        return self.title
+    
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+
+    def __str__(self):
+        return self.name
