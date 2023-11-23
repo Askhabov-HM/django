2aab31126d17a0050780101467a43ff26c065ced
Askhabov-HM <askhamzat@mail.ru>
2023-11-23 22:31:49 +0300

    #T-5 add user field for Todo

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Todo(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
     category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+    user = models.ForeignKey(User, verbose_name='User', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
